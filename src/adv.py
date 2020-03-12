7d0a38e66c13b2b6c8358710489e406f9200692d
TheHaunted <kevin_lopez@live.com>
2020-03-12 16:11:01 -0500

    MVP

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,25 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", ["sword", "knife"]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ["pistol"]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", ["laser", "battery"]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", ["water"]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""")
 }
 
 
@@ -36,8 +37,16 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+def print_items():
+    print('Items:\n')
+    for item in player.room.items:
+        print(item)
 
+finished = False
 # Make a new player object that is currently in the 'outside' room.
+player = Player(room['outside'])
+print(player.room.description)
+print_items()
 
 # Write a loop that:
 #
@@ -49,3 +58,27 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while not finished:
+    command = input("-> ")
+    if command == 'q':
+        finished = True
+    if command.lower() == 'north':
+        player.move('north')
+    if command.lower() == 'south':
+        player.move('south')
+    if command.lower() == 'west':
+        player.move('west')
+    if command.lower() == 'east':
+        player.move('east')
+
+    if 'take' in command.lower():
+        # command_to_list = command.lower().split()
+        # player.take_item(command_to_list[1:])
+        player.take_item(command.lower())
+        
+    if command.lower() == 'items':
+        player.room_items()
+
+    if command.lower() == 'inventory':
+        player.inventory()
--- src/player.py
+++ src/player.py
@@ -1,2 +1,47 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, room, items=[]):
+        self.room = room
+        self.items = items
+    def move(self, dir):
+        if dir.lower() == 'north':
+            if self.room.n_to != None:
+                self.room = self.room.n_to
+                print(self.room.description)
+            else:
+                print("There's nothing there. You head back.")
+        if dir.lower() == 'south':
+            if self.room.s_to != None:
+                self.room = self.room.s_to
+                print(self.room.description)
+            else:
+                print("There's nothing there. You head back.")
+        if dir.lower() == 'east':
+            if self.room.e_to != None:
+                self.room = self.room.e_to
+                print(self.room.description)
+            else:
+                print("There's nothing there. You head back.")
+        if dir.lower() == 'west':
+            if self.room.w_to != None:
+                self.room = self.room.w_to
+                print(self.room.description)
+            else:
+                print("There's nothing there. You head back.")
+
+    def room_items(self):
+        for item in self.room.items:
+            print(item)
+    
+    def take_item(self, command):
+        for item in self.room.items:
+            if item in command.lower():
+                self.items.append(item)
+                self.room.items.remove(item)
+                print(f'Took {item}')
+        
+    def inventory(self):
+        for item in self.items:
+            print(item)
